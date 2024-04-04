5d2116c49b2f1d8b6d69ec7542718f83d21f9fae
DylanLev <dylan.levy5@yahoo.com>
2024-04-04 20:38:18 +0300

    Modules

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 //Dont reveal confidential info for shared projects
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 
 const app = express();
@@ -11,6 +13,19 @@ const PORT = process.env.PORT || 8080
 
 //Show the requests logs
 app.use(morgan('tiny'));
+
+//Parse request to body-parser
+app.use(bodyparser.urlencoded({extended: true}));
+
+//Set view engine
+app.set("view engine","ejs");
+//app.set("views",path.resolve(__dirname, "views/ejs"));
+
+//Load assets
+//Make it easier to use css files (ex: /css/style.css) instead of writing all the route assets/css
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
 
 app.get('/', (req,res)=>{
     res.send('Crud app');
